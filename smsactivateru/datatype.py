b2ad684641e6f8be2b07786b5ae42a15bd898358
SHELA <sheludko@vladyslav.ru>
2023-01-22 21:54:55 +0200

    Update datatype.py

--- smsactivateru/datatype.py
+++ smsactivateru/datatype.py
@@ -11,8 +11,8 @@ class SmsTypes:
 		MM = '5'  # Мьянма (Myanmar)
 		ID = '6'  # Индонезия (Indonesia)
 		MY = '7'  # Малайзия (Malaysia)
-		# KE = '8'  # Кения (Kenya) – temp disable
-		# TZ = '9'  # Танзания (Tanzania) – temp disable
+		KE = '8'  # Кения (Kenya) – temp disable
+		TZ = '9'  # Танзания (Tanzania) – temp disable
 		VN = '10'  # Вьетнам (Vietnam)
 		KG = '11'  # Кыргызстан (Kyrgyzstan)
 		US_VIRT = '12'  # США ВИРТ (USA)
@@ -49,6 +49,26 @@ class SmsTypes:
 		HR = '45'  # Хорватия ( Croatia)
 		IQ = '47'  # Ирак (Iraq)
 		NL = '48'  # Нидерланды (Netherlands)
+		TH = '52'  # Таиланд (Thailand)
+		SA = '53'  # Саудовская Аравия (Saudi Arabia)
+		MX = '54'  # Мексика (Mexico)
+		TW = '55'  # Тайвань (Taiwan)
+		ES = '56'  # Испания (Spain)
+		IR = '57'  # Иран (Iran)
+		DZ = '58'  # Алжир (Algeria)
+		SI = '59'  # Словения (Slovenia)
+		BD = '60'  # Бангладеш (Bangladesh)
+		SN = '61'  # Сенегал (Senegal)
+		TR = '62'  # Турция (Turkey)
+		CZ = '63'  # Чехия (Czechia)
+		LK = '64'  # Шри-Ланка (Sri Lanka)
+		PE = '65'  # Перу (Peru)
+		PK = '66'  # Пакистан (Pakistan)
+		NZ = '67'  # Новая Зеландия (New Zealand)
+		GN = '68'  # Гвинея (Guinea)
+		ML = '69'  # Мали (Mali)
+		VE = '70'  # Венесуэла (Venezuela)
+		ET = '71'  # Эфиопия (Ethiopia)
 		BR = '73'
 		US = '187' # США
 
